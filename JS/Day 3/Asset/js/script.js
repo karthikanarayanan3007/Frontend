358b36e5294cc0da5f8cece3d1622456a61adc7f
karthi <karthikanarayanan3007@gmail.com>
2026-09-23 22:04:58 +0530

    “update”

--- JS/Day 3/Asset/js/script.js
+++ JS/Day 3/Asset/js/script.js
@@ -29,10 +29,145 @@ let valuesix=10
 valu=valuefive+=valuesix
 console.log(val);
 
+//task-1
+var foodOne="briyani"
+var foodTwo="idly"
+var foodThree="dosa"
+var foodFour="rice"
+var foodFive="noodles"
+var foodSix="shawarma"
+var foodSeven="chapathi"
+var foodEight="pori"
+var foodNine="tomato rice"
+var foodTen="lemon rice"
+var foodEleven="veg briyani"
+var foodTwelve="porata"
+var foodThirteen="idiyapam"
+var foodFourteen="nonveg meal"
+var foodFifteen="veg meal"
+var foodSixteen="sambar satham"
+var foodSeventeen="curt rice"
+var foodEighteen="rasam rice"
+
+console.log(foodOne)
+console.log(foodTwo)
+console.log(foodThree)
+console.log(foodFour)
+console.log(foodFive)
+console.log(foodSix)
+console.log(foodSeven)
+console.log(foodEight)
+console.log(foodNine)
+console.log(foodTen)
+console.log(foodEleven)
+console.log(foodTwelve)
+console.log(foodThirteen)
+console.log(foodFourteen)
+console.log(foodFifteen)
+console.log(foodSixteen)
+console.log(foodSeventeen)
+console.log(foodEighteen)
+
+
+//task-2
+var foodOne="briyani"
+foodOne+="pongal"
+console.log(foodOne);
+
+var foodTwo="idly"
+foodTwo-="maggie"
+console.log(foodTwo);
+
+var foodThree="idly"
+foodThree*="pasta"
+console.log(foodThree);
+
+var foodThree="idly"
+foodThree/="pasta"
+console.log(foodThree);
+
+var foodThree="idly"
+foodThree%="pasta"
+console.log(foodThree);
+
+var foodThree="idly"
+foodThree**="pasta"
+console.log(foodThree);
 
 
 
+//task-3
+var timeOne=1
+var timeTwo=2
+var timeThree=3
+var timeFour=4
+var timeFive=5
+var timeSix=6
+var timeSeven=7
+var timeEight=8
+var timeNine=9
+var timeTen=10
+var timeEleven=11
+var timeTwelve=12
+var timeThirteen=13
+var timeFourteen=14
+var timeFifteen=15
+var timeSixteen=16
+var timeSeventeen=17
+var timeEighteen=18
 
+console.log(timeOne);
+console.log(timeTwo);
+console.log(timeThree);
+console.log(timeFour);
+console.log(timeFive);
+console.log(timeSix);
+console.log(timeSeven);
+console.log(timeEight);
+console.log(timeNine);
+console.log(timeTen);
+console.log(timeEleven);
+console.log(timeTwelve);
+console.log(timeThirteen);
+console.log(timeFourteen);
+console.log(timeFifteen);
+console.log(timeSixteen);
+console.log(timeSeventeen);
+console.log(timeEighteen);
 
+//task-4
+var timeOne=1
+var timeTwo=2
+var timeTHree=3
+timeOne=timeTwo=timeThree
+console.log(timeOne)
 
+var timeOne=1
+var timeTwo=2
+value=timeOne+=timeTwo
+console.log(value)
 
+var timeOne=1
+var timeTwo=2
+value=timeOne-=timeTwo
+console.log(value)
+
+var timeOne=1
+var timeTwo=2
+value=timeOne*=timeTwo
+console.log(value)
+
+var timeOne=1
+var timeTwo=2
+value=timeOne/=timeTwo
+console.log(value)
+
+var timeOne=1
+var timeTwo=2
+value=timeOne%=timeTwo
+console.log(value)
+
+var timeOne=1
+var timeTwo=2
+value=timeOne**=timeTwo
+console.log(value)
